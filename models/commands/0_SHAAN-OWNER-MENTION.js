ee99f3ea0445269c1078559f78bd1e639ad32ad1
Shaanabot <shaankhank325@gmail.com>
2026-02-11 15:04:44 +0500

    Update and rename 0_ARIF-OWNER-MENTION.js to 0_SHAAN-OWNER-MENTION.js

---
+++ models/commands/0_SHAAN-OWNER-MENTION.js
@@ -0,0 +1,31 @@
+module.exports.config = {
+  name: "ADMIN",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "SHAAN BABU",
+  description: "THIS BOT WAS MADE BY MR SHAAN BABU",
+  commandCategory: "ADMIN MENTION",
+  usages: "",
+  cooldowns: 1
+};
+module.exports.handleEvent = function({ api, event }) {
+  if (event.senderID !== "100016828397863") {
+];
+    for (const id of aid) {
+    if ( Object.keys(event.mentions) == id) {
+      var msg = ["Arey yaar mujhse baat karo, boss ko kyun bula rahe ho 🙄"
+"Kya hua, mere boss ko kyun bula rahe ho?"
+"Mere boss shayad busy hain."
+"Arey yaar kyun mere boss ko tang kar rahe ho?"
+"Mera boss abhi so rahe honge."
+"Lagta hai mera boss babu sona karne mein busy hai 🤔"
+"Kya kaam hai mujhe batao, main boss ko bata dunga."
+"Mere boss shayad abhi offline hain."
+"Mere boss abhi busy hain 😐✌️
+"];
+      return api.sendMessage({body: msg[Math.floor(Math.random()*msg.length)]}, event.threadID, event.messageID);
+    }
+    }}
+};
+module.exports.run = async function({}) {
+        }
