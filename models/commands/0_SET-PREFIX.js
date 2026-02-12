8142c9dfdf17d5b503ab73b313bdd1f338329874
Shaanabot <shaankhank325@gmail.com>
2026-02-12 12:30:41 +0500

    Update 0_SET-PREFIX.js

--- models/commands/0_SET-PREFIX.js
+++ models/commands/0_SET-PREFIX.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "setprefix",
 	version: "1.0.1",
 	hasPermssion: 2,
-	credits: "ARIF BABU",
+	credits: "SHAAN BABU",
 	description: "Reset group prefix",
 	commandCategory: "System",
 	usages: "[prefix/reset]",
