73fa096e7d1729db13960b02a479183facc99deb
Shaanabot <shaankhank325@gmail.com>
2026-02-12 11:34:43 +0500

    Update and rename 0_ARIF-PAIR-0.js to 0_SHAAN-PAIR-0.js

--- models/commands/0_SHAAN-PAIR-0.js
+++ models/commands/0_SHAAN-PAIR-0.js
@@ -1,11 +1,11 @@
-const PREM_BABU= "PREM-PROJECT"; // Original credit
+const SHAAN_BABU = "Shaan Babu"; // Variable name bhi change kar diya gaya hai
 
 module.exports.config = {
   name: "pair",
   version: "1.0.0", 
   hasPermssion: 0,
-  credits: PREM_BABU,
-  description: "THIS BOT WAS MADE BY MR PREM BABU",
+  credits: "SHAAN_BABU"
+  description: "THIS BOT WAS MADE BY SHAAN BABU",
   commandCategory: "LOVE PAIRING",
   usePrefix: false,
   usages: "PREFIX", 
@@ -18,21 +18,20 @@ module.exports.run = async function({ api, event, Threads, Users }) {
 
   var { participantIDs } = (await Threads.getData(event.threadID)).threadInfo;
   var tle = Math.floor(Math.random() * 101);
-  var namee = (await Users.getData(event.senderID)).name
+  var namee = (await Users.getData(event.senderID)).name;
   const botID = api.getCurrentUserID();
   const listUserID = event.participantIDs.filter(ID => ID != botID && ID != event.senderID);
   var id = listUserID[Math.floor(Math.random() * listUserID.length)];
-  var name = (await Users.getData(id)).name
+  var name = (await Users.getData(id)).name;
   var arraytag = [];
   arraytag.push({id: event.senderID, tag: namee});
   arraytag.push({id: id, tag: name});
 
-  // Check if credit has been modified
-  if (module.exports.config.credits !== PREM_BABU) {
-    return api.sendMessage("DON'T CHANGE CREDIT! PREM-PROJECT FUCK YOUR MOTHER AND SISTER", event.threadID, event.messageID);
+  // Credit protection check updated to Shaan Babu
+  if (module.exports.config.credits !== SHAAN_BABU) {
+    return api.sendMessage("DON'T CHANGE CREDIT! THIS BOT IS MAINTAINED BY SHAAN BABU", event.threadID, event.messageID);
   }
 
-  // Array of Imgur gif links
   const imgurGifs = [
     "https://i.imgur.com/41ogamD.gif",
     "https://i.imgur.com/qVpA7RB.gif",
@@ -40,24 +39,19 @@ module.exports.run = async function({ api, event, Threads, Users }) {
     "https://i.imgur.com/3qSoi8K.gif",
     "https://i.imgur.com/EO5EtJp.gif",
     "https://i.imgur.com/fP5kc0h.gif"
-    // Add more Imgur gif links as needed
   ];
 
-  // Select a random Imgur gif link
   const randomImgurLink = imgurGifs[Math.floor(Math.random() * imgurGifs.length)];
 
-  // Download the gif from the selected Imgur link
   const gifData = (await axios.get(randomImgurLink, { responseType: "arraybuffer" })).data; 
   fs.writeFileSync(__dirname + "/cache/giflove.png", Buffer.from(gifData, "utf-8"));
 
-  // Download avatars
   let Avatar = (await axios.get( `https://graph.facebook.com/${event.senderID}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data; 
   fs.writeFileSync( __dirname + "/cache/avt.png", Buffer.from(Avatar, "utf-8") );
 
   let Avatar2 = (await axios.get( `https://graph.facebook.com/${id}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: "arraybuffer" } )).data;
   fs.writeFileSync( __dirname + "/cache/avt2.png", Buffer.from(Avatar2, "utf-8") );
 
-  // Prepare attachments
   var imglove = [];
   imglove.push(fs.createReadStream(__dirname + "/cache/avt.png"));
   imglove.push(fs.createReadStream(__dirname + "/cache/giflove.png"));
